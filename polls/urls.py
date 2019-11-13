09cd5a148d6708c9e90fbfc43a0cff189ae88fd0
YSJ <theblesedcity@hanmail.net>
2019-11-13 19:55:00 +0900

    ENH : polls URL 확인

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('<int:question_id>/', detail, name="detail"),
+    path('<int:question_id>/result', result, name="result"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('hello world')
+
+def detail(request, question_id):
+    return HttpResponse('hello detail' + str(question_id))
+
+def result(request, question_id):
+    return HttpResponse('hello result' + str(question_id))
